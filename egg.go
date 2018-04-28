b8d1b0163820f2a65695f25f917d9d6827ab4bab
pallat <yod.pallat@gmail.com>
2018-04-28 16:28:25 +0700

    chan intent

--- egg.go
+++ egg.go
@@ -6,7 +6,7 @@ type Responder func() <-chan interface{}
 
 func New(w Worker) Responder {
 	resp := make(chan interface{})
-	go func(ch chan interface{}) {
+	go func(ch chan<- interface{}) {
 		defer func() {
 			if r := recover(); r != nil {
 				resp <- r
